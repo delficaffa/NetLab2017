151bb14286825a729d39678e3a0b950735824de7
delfi <md.caffa@hotmail.com>
2017-11-30 14:04:13 -0300

    Clase de archivos..

---
+++ Archivos/IntroArchivos/IntroArchivos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"mytest.txt";
+            FileStream fileStr = File.Create(fileName);
+        }
+    }
+}
