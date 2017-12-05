6d59faf40f2f5bc3af87aab10e317259a076660d
delfi <md.caffa@hotmail.com>
2017-12-05 13:24:36 -0300

    Los primeros ejercicios ahora en web

---
+++ PracticaWebCesar/PracticaWeb/Controllers/Ejercicio1Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaWeb.Controllers
+{
+    public class Ejercicio1Controller : Controller
+    {
+        // GET: Ejercicio1
+        public ActionResult Sumar()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Sumar(int a,int b, int c)
+        {
+            var resultado = a + b + c;
+
+            ViewData.Add("resultado", resultado);
+
+            return View();
+            
+        }
+        
+
+    }
+}
