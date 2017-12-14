5c4051d8c66b7c00a0370ff6e4944a52448db2c3
Delfi Caffa <md.caffa@hotmail.com>
2017-12-14 12:20:30 -0300

    Agregue el Login al ejercicio de webNoticias

--- 07.12PracticaNoticias/WebNoticias/Controllers/HomeController.cs
+++ 07.12PracticaNoticias/WebNoticias/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebNoticias.Filters;
 
 namespace WebNoticias.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,6 +15,7 @@ namespace WebNoticias.Controllers
             return View();
         }
 
+        [AuthorizationFilterAtribute]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,10 +23,31 @@ namespace WebNoticias.Controllers
             return View();
         }
 
+        [AuthorizationFilterAtribute]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+           return View();
+        }
+
+        [AuthorizationFilterAtribute]
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            ControllerContext.HttpContext.Session["LoggedUser"] = username;
+            return View();
+        }
+        [AuthorizationFilterAtribute]
+        [HttpPost]
+        public ActionResult LogOut()
+        {
+            ControllerContext.HttpContext.Session.Remove("LoggedUser");
             return View();
         }
     }
---
+++ 07.12PracticaNoticias/WebNoticias/Filters/AuthorizationFilterAtribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebNoticias.Filters
+{
+    public class AuthorizationFilterAtribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var loggedUser = filterContext.HttpContext.Session["LoggedUser"];
+
+            if (loggedUser == null)
+            {
+                filterContext.Result = new RedirectResult("/Home/Login");
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+
+        }
+    }
+}
